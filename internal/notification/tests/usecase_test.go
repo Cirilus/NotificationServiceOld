0c5eec33d4618ec06949022a8c550ef1d3d77a77
Kirill <shkolenko02-03-2004@mail.ru>
2023-07-25 09:23:44 +0300

    Add the mocks and rebuild the folder architecture of the test

--- internal/notification/tests/usecase_test.go
+++ internal/notification/tests/usecase_test.go
@@ -1,1 +1,28 @@
 package tests
+
+import "testing"
+
+func TestCreateNotification(t *testing.T) {
+
+}
+func TestAddNotificationToAccount(t *testing.T) {
+
+}
+func TestDeleteNotificationFromAccount(t *testing.T) {
+
+}
+func TestDeleteNotification(t *testing.T) {
+
+}
+func TestUpdateNotifications(t *testing.T) {
+
+}
+func TestNotificationById(t *testing.T) {
+
+}
+func TestAllNotifications(t *testing.T) {
+
+}
+func TestNotificationsByUser(t *testing.T) {
+
+}
